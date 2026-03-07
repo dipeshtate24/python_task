e164cd613456be504daa8774a1a63da9233eda09
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-07 16:57:50 +0530

    Add files via upload

---
+++ Variable_Data_type.py
@@ -0,0 +1,38 @@
+a = "3534"
+# a = "35fgrfg4"
+a = int(a)
+print(type(a))
+print(a+5)
+
+a = input("Enter number:")
+a = int(a)
+print(type(a))
+
+## Practice
+a = 30
+b = 15
+print("The sum of a and b is", a+b)
+
+## Write a programm reminder divided by 
+a = 30
+b = 15
+print("The reminder when is a divided by b is", a%b)
+
+## Check the type of variable assigned input()
+a = input("Enter number here:")
+print(type(a))
+
+## Use comprison operator find the a is grater b or not
+a = 35
+b = 80
+print("a is grater than b or not is", a > b) 
+
+## Take two number form person and find the average of that number
+a = int(input("Enter your first number:"))
+b = int(input("Enter your second number"))
+print("The average of two number given is", (a+b)/2)
+
+
+## Take one number from user and square that number 
+a = int(input("Enter your number:"))
+print("The average of two number given is", a**2)
