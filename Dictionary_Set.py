36a004fa9c9b29d681ab17cbdc3d6beb5fc10b9f
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-09 23:18:38 +0530

    Update Dictionary_Set.py

--- Dictionary_Set.py
+++ Dictionary_Set.py
@@ -1,7 +1,22 @@
 myDict = {
     'Fruits':'Apple',
     'Game': 'Chess',
-    
+    'person': {'Name': 'Suresh'}
 }
 
 print(myDict['Game'])
+print(myDict['person']['Name'])
+print(list(myDict.keys()))
+print(myDict.items())
+print(myDict.values())
+
+
+update_Dict = {
+    "Lovish": 'Friend',
+    'shubham': 'Friend'
+}
+
+myDict.update(update_Dict)
+print(myDict)
+
+print(myDict.get('Fruits'))
