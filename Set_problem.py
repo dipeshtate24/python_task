77f5c8757e13258f2cf1f03a7be2c7cb938b44bd
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-10 23:55:08 +0530

    Update Set_problem.py

--- Set_problem.py
+++ Set_problem.py
@@ -6,4 +6,23 @@ print(a)
 b = set()
 b.add(4)
 b.add(5)
+
 print(b)
+
+print(b.add({4:5}))
+
+print(len(b))
+
+print(b.pop())
+
+
+Dict_Book ={
+  "Pankha": "Fan",
+  "Dabba": "Box"
+
+}
+
+print('Option are', Dict_Book.keys())
+a = input('Enter Hindi word\n')
+# print("The meaning of your word is:", Dict_Book[a])
+print("The meaning of your word is:", Dict_Book.get(a))
