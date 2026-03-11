7ed1fb0d2de2c103d095769c59c8dec65cc5e204
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-11 23:36:13 +0530

    Update Set_problem.py

--- Set_problem.py
+++ Set_problem.py
@@ -26,3 +26,29 @@ print('Option are', Dict_Book.keys())
 a = input('Enter Hindi word\n')
 # print("The meaning of your word is:", Dict_Book[a])
 print("The meaning of your word is:", Dict_Book.get(a))
+
+s= {18}
+print(type(int(s))) # Error is int() argument must be a string, a bytes-like object or a real number, not 'set'
+print(type(str(s)))
+
+
+s = set()
+s.add(20)
+s.add(20.0)
+s.add('20')
+print(len(s))
+
+s = {}
+print(type(s))
+
+Dict_language = {}
+Name = input("Enter your name:")
+language = input("Enter your favourite langauge:")
+Dict_language[Name] = language
+Name = input("Enter your name:")
+language = input("Enter your favourite langauge:")
+Dict_language[Name] = language
+print(Dict_language)
+
+s = {8, 7, 12, "Viky", [1,2]} # Error is unhashable type: 'list'
+print(s[1])
