87d493fabe647c42c9efe6598b4f0aafac2ff85c
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-12 23:43:39 +0530

    Add files via upload

---
+++ conditional_experssion.py
@@ -0,0 +1,30 @@
+a = 22 
+if (a > 9):
+    print('grater')
+else:
+    print('lesser')
+
+age = int(input("Enter your age:"))
+if (age > 18):
+    print("Your are eligible")
+else:
+    print("Your are not eligible")
+
+a = None
+if a is None:
+    print('Yes')
+else:
+    print('No')
+
+
+fruits = ['apple', 'mango']
+
+if 'apple' in fruits:
+    print('Yes')
+else:
+    print('No')
+
+if not 'Banana' in fruits:
+    print("banana is not in fruits list.")
+else:
+    print("present in the fruits list.") 
