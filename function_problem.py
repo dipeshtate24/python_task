da34eae4ef9cc500dd03bdd02125bf61af2a5535
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-15 16:34:57 +0530

    Add files via upload

---
+++ function_problem.py
@@ -0,0 +1,83 @@
+def percentage(marks):
+    average = (sum(marks)/(len(marks) * 100)) * 100
+    return average
+
+p = percentage([75, 98, 88, 78])
+print(p)
+
+
+def greet(name):
+    return "Hello, Good Day" + " " + name
+print(greet('Manish'))
+
+def mysum(num1, num2):
+    return num1+num2
+
+print(mysum(5, 10))
+
+
+def factorial(n):
+    total = 1
+    for i in range(1, n+1):
+        total *= i
+    return total
+print(factorial(5))
+
+
+def recersive_fact(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * recersive_fact(n-1)
+    
+print(recersive_fact(5))
+
+
+def maximum(num1, num2, num3):
+    if num1 > num2:
+        num_new1 = num1
+    else:
+        num_new1 = num2
+
+    if num_new1 > num3:
+        return num_new1
+    else:
+        return num3
+
+print(maximum(45, 98, 78))
+
+
+
+def farh(c):
+    return c*(9/5) + 32
+
+print(farh(45))
+
+print("Hello", end="\n")
+print("are", end="\n")
+print("you!", end="\n")
+
+def fab(n):
+    for i in range(5):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        else:
+            return fab(n-1) + fab(n-2)
+
+print(fab(5))
+
+def natural_sum(n):
+    if n == 1:
+        return 1
+    else:
+        return n + natural_sum(n-1)
+
+print(natural_sum(5)) 
+
+new_list = ["apple", "mango"]
+for i in new_list:
+    if "apple" == i:
+        new_list.remove(i)
+    print(new_list) 
