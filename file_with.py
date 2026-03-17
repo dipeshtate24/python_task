bd14fb9a9178b121104298f1c7bd9b8a51d18e0f
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-17 23:56:38 +0530

    Update file_with.py

--- file_with.py
+++ file_with.py
@@ -5,3 +5,12 @@ with open('another.txt', 'w') as f:
     a = f.write('I like mangoes.')
 
 print(a)
+
+
+with open('another.txt', 'r') as f:
+    a = f.readline(5)
+
+with open('another.txt', 'a') as f:
+    a = f.write('i am appending')
+
+print(a)
