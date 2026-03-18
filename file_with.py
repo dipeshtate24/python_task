0018b047f092a433f04a31dadc3a6bd05fdcd16b
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-18 23:53:30 +0530

    Update file_with.py

--- file_with.py
+++ file_with.py
@@ -14,3 +14,21 @@ with open('another.txt', 'a') as f:
     a = f.write('i am appending')
 
 print(a)
+
+f=open('find_word.txt')
+a = f.read()
+if "Twinkle" in a:
+    print("Given word in present in the file.")
+else:
+    print("Given word not present in the file.")
+
+f.close()
+
+new_number = 78
+
+with open('number.txt','r') as f:
+    a = int(f.read())
+if new_number > 0 and new_number > a :
+    with open('number.txt', 'w') as f:
+        f.write(str(new_number))
+
