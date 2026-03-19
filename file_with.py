1a385e92a0fba1c7ef794f35e49b401d1a2ef3e3
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-19 23:09:58 +0530

    Update file_with.py

--- file_with.py
+++ file_with.py
@@ -33,8 +33,75 @@ if new_number > 0 and new_number > a :
         f.write(str(new_number))
 
 
+for i in range(2, 21):
+    with open(f'Mulitiplication_of_{i}.txt', 'w') as f:
+        for j in range(1,11):
+            f.write(f"{i}*{j}={i * j}\n")
+    break
+
+words = ['donkey', 'kaddu', 'mote']
 
 
+with open('word.txt') as f:
+    content = f.read()
 
-print("hello")
+for word in words:
+    content = content.replace(word, f"{'#' * len(word)}")
 
+    with open('word.txt', 'w') as f:
+        f.write(content)
+
+
+with open('log.txt') as f:
+    content = f.read()
+
+word = 'python'
+if word in content.lower():
+    print("python is present.")
+else:
+    print("python is not present.")
+
+word = 'python'
+content = True
+i = 1
+with open('log.txt') as f:
+    while content:
+        content = f.readline()
+
+        if word in content.lower():
+            print(f"python is present in line {i}")
+        i += 1
+        
+
+with open('this.txt') as f:
+    content = f.read()
+
+with open('copy.txt', 'w') as f:
+    f.write(content)
+
+with open('this.txt') as f:
+    content1 = f.read()
+
+with open('copy.txt') as f:
+    content2 = f.read()
+
+if content1 == content2:
+    print("Both file content is identical.")
+else:
+    print("Both file content is identical.")
+
+filename = "clean_file.txt"
+
+with open(filename, 'w') as f:
+    f.write("")
+
+import os 
+oldname = "clean_file2.txt"
+new_filename = "renamed_by_python.txt"
+with open(oldname) as f:
+     content = f.read()
+
+with open(new_filename, 'w') as f:
+     f.write(content)
+
+os.remove(oldname)
