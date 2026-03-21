9ec8826bccc574584ded24257358da4c59642d35
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-21 23:26:24 +0530

    Update oop.py

--- oop.py
+++ oop.py
@@ -27,3 +27,90 @@ Application.name = "Soham"
 Application.train = "Rajdhani Express"
 
 Application.printData()
+
+
+class Employee():
+    company = "Google"
+    salary = 100
+
+Suresh = Employee()
+Rajni = Employee()
+
+# Suresh.salary = 300
+# Rajni.salary = 400
+
+print(Suresh.company)
+print(Rajni.company)
+
+# Employee.company = "Youtube"
+
+# print(Suresh.company)
+# print(Rajni.company)
+Suresh.salary = 45
+print(Suresh.salary)
+print(Rajni.salary)
+
+
+class Employee:
+    company = 'Google'
+    def getSalary(self):
+        print(f"Salary for this employee working in {self.company} and salary is {self.salary}")
+
+    def greet(self):
+        print(f'Good Morning, {self.name}')
+
+
+Person = Employee()
+Person.salary = 100000
+Person.name = 'Suresh'
+Person.getSalary()
+Person.greet()
+
+class Employee:
+    company = 'Google'
+    def getSalary(self, signature):
+        print(f"Salary for this employee working in {self.company} and salary is {self.salary}.\n{signature}")
+
+    @staticmethod
+    def greet():
+        print(f'Good Morning, Everyone')
+
+    @staticmethod
+    def time():
+        print("The time is 9 AM in the morning")
+
+Person = Employee()
+Person.salary = 100000
+Person.name = 'Suresh'
+Person.getSalary("Thanks!")
+Person.greet()
+Person.time()
+
+
+class Employee:
+    company = 'Google'
+
+    def __init__(self, name, salary, subnit):
+        self.name = name
+        self.salary = salary
+        self.subnit = subnit
+        print("Employee is created.")
+
+    def getDetails(self):
+        print(f"The name of the employee is {self.name}")
+        print(f"The salary of the employee is {self.salary}")
+        print(f"The subnit of the employee is {self.subnit}")
+    
+    def getSalary(name, salary):
+        print(f"Salary for this employee working in {name} and salary is {salary}.")
+
+    @staticmethod
+    def greet():
+        print(f'Good Morning, Everyone')
+
+    @staticmethod
+    def time():
+        print("The time is 9 AM in the morning")
+
+Person1 = Employee("Ramesh", 100, "Youtube")
+Person1.getDetails()
