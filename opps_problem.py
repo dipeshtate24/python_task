66fad10e89b27492b06f651c77bddf3fdd09d174
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-03-24 23:33:46 +0530

    Update opps_problem.py

--- opps_problem.py
+++ opps_problem.py
@@ -1,24 +1,24 @@
-# class C2dvec:
-#     def __init__(self, i, j):
-#         self.icap = i
-#         self.jcap = j
+class C2dvec:
+    def __init__(self, i, j):
+        self.icap = i
+        self.jcap = j
 
-#     def __str__(self):
-#         return f"{self.icap + self.jcap}"
+    def __str__(self):
+        return f"{self.icap + self.jcap}"
 
-# class C3dvec(C2dvec):
-#     def __init__(self, i, j, k):
-#         super().__init__(i, j)
-#         self.kcap = k
+class C3dvec(C2dvec):
+    def __init__(self, i, j, k):
+        super().__init__(i, j)
+        self.kcap = k
     
-#     def __str__(self):
-#         return f"{self.icap + self.jcap + self.kcap}"
+    def __str__(self):
+        return f"{self.icap + self.jcap + self.kcap}"
 
 
-# v2d = C2dvec(1, 3)
-# v3d = C3dvec(1, 9, 7)
-# print(v2d)
-# print(v3d)
+v2d = C2dvec(1, 3)
+v3d = C3dvec(1, 9, 7)
+print(v2d)
+print(v3d)
 
 
 class Animals:
@@ -34,3 +34,52 @@ class Dog:
     
 d = Dog()
 d.bark()
+
+class Employee:
+    salary = 1000
+    increment = 1.5
+
+    @property
+    def salaryAfterIncrement(self):
+        return self.salary * self.increment
+    
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, sai):
+        self.increment = sai/self.salary
+
+e = Employee()
+print(e.increment)
+print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 2000
+e.increment = 2
+print(e.salaryAfterIncrement)
+
+class Complex:
+    def __init__(self, r, i):
+        self.real = r
+        self.imaginary = i
+
+    def __add__(self, c):
+        return Complex(self.real+c.real, self.imaginary+c.imaginary)
+    
+    def __mul__(self, c):
+        mulReal = (self.real * c.real) - (self.imaginary * c.imaginary)
+        # mulImg = (self.real * c.imaginary) - (self.imaginary * c.real)
+        mulImg = (self.real * c.imaginary) + (self.imaginary * c.real)
+
+        return Complex(mulReal, mulImg)
+    
+    def __str__(self):
+        if self.imaginary<0:
+            return f"{self.real} - {-self.imaginary}"
+        else:
+            return f"{self.real} + {self.imaginary}i"
+
+# c1 = Complex(8, 5)
+# c2 = Complex(1, 4)
+c1 = Complex(333, 2)
+c2 = Complex(1, 37)
+print(c1)
+print(c2)
+print(c1+c2)
+print(c1*c2)
