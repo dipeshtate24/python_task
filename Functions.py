0b14c2b48ad41c57a4f95549389f26c17114b1c4
dipeshtate24 <dipeshtate24@gmail.com>
2026-04-04 16:16:06 +0530

    Add New Files Fucntions and debugging

--- Functions.py
+++ Functions.py
@@ -10,42 +10,42 @@
 
 # print('cats', 'dogs', 'mice', sep=',')
 
-# def spam(divide_by):
-#     try:
-#         return 42/divide_by
+def spam(divide_by):
+    try:
+        return 42/divide_by
     
-#     except ZeroDivisionError:
-#         print('Error: Invalid argument')
+    except ZeroDivisionError:
+        print('Error: Invalid argument')
 
-# print(spam(2))
-# print(spam(12))
-# print(spam(0))
-# print(spam(1))
+print(spam(2))
+print(spam(12))
+print(spam(0))
+print(spam(1))
 
-# def spam(divide_by):
-#     return 42/divide_by
+def spam(divide_by):
+    return 42/divide_by
 
-# try:
-#     print(spam(2))
-#     print(spam(12))
-#     print(spam(0))
-#     print(spam(1))
+try:
+    print(spam(2))
+    print(spam(12))
+    print(spam(0))
+    print(spam(1))
 
     
-# except ZeroDivisionError:
-#     print('Error: Invalid argument')
+except ZeroDivisionError:
+    print('Error: Invalid argument')
 
-# import random 
-# # random_number = random.randint(1, 6)
+import random 
+# random_number = random.randint(1, 6)
 
-# def get_random_dice_roll():
-#     random_number = random.randint(1, 6)
-#     return random_number
+def get_random_dice_roll():
+    random_number = random.randint(1, 6)
+    return random_number
 
-# print(get_random_dice_roll())
-# print(get_random_dice_roll())
-# print(get_random_dice_roll())
-# print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
 
 
 print("Enter integer Number")
