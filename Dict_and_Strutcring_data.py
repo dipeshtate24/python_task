e6dad7338fbc92a055b58a39f797034802678931
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-04-07 23:22:01 +0530

    Update Dict_and_Strutcring_data.py

--- Dict_and_Strutcring_data.py
+++ Dict_and_Strutcring_data.py
@@ -1,11 +1,11 @@
-# message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
-# count = {}
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
 
-# for character in message:
-#     count.setdefault(character, 0)
-#     count[character] += 1
+for character in message:
+    count.setdefault(character, 0)
+    count[character] += 1
 
-# print(count)
+print(count)
 
 all_guests = {'Alice': {'apples': 5, 'pretzels': 12},
               'Bob': {'ham sandwiches': 3, 'apples': 2},
@@ -23,3 +23,42 @@ print(' - Cups           ' + str(total_brought(all_guests, 'cups')))
 print(' - Cakes          ' + str(total_brought(all_guests, 'cakes')))
 print(' - Ham Sandwiches ' + str(total_brought(all_guests, 'ham sandwiches')))
 print(' - Apple Pies     ' + str(total_brought(all_guests, 'apple pies')))
+
+
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def display_inventory(inventory):
+    print("Inventory")
+    
+    items_total = 0
+    for k, v in inventory.items():
+        items_total += v
+    print("Total number of items:" + str(items_total))
+
+display_inventory(stuff)
+
+
+def add_to_inventory(inventory, added_item):
+
+    for i in added_item:
+        if i in inventory:
+            inventory[i] += 1
+        else:
+            inventory[i] = 1
+
+    print(inventory)
+
+def display_inventory(inventory):
+    print("Inventory")
+    
+    items_total = 0
+    for k, v in inventory.items():
+        print(f"{v} {k}")
+        items_total += v
+    print("Total number of items:" + str(items_total))
+
+inv = {'gold coin': 42, 'rope': 1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+add_to_inventory(inv, dragon_loot)
+display_inventory(inv)
+
