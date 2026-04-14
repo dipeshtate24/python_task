e3b473626c8e6eef400a3979025897e725bdfc9f
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-04-14 23:20:32 +0530

    Add For loop file

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+colors = ["Red", "Green", "Blue", "Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for i in range(4, 10):
+    print(i)
+
+
+for i in range(1, 12, 3):
+    print(i)
