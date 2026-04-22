9c6d07ad5cf8c85ec4755b51463a0dc181e7461e
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-04-22 22:38:46 +0530

    Add error handling file

---
+++ error_handling.py
@@ -0,0 +1,49 @@
+a = int(input("Enter any value between 5 and 9 :"))
+
+if a<5 or a>9:
+    raise ValueError("Value should be between 5 and 9")
+
+import sys
+
+try:
+    f = open('myfile.txt')
+    s = f.readline()
+    i = int(s.strip())
+
+except OSError as err:
+    print("OS error:", err)
+except ValueError:
+    print("Could not convert data to an integer.")
+except Exception as err:
+    print(f"Unexpected {err=}, {type(err)=}")
+    raise
+
+class B(Exception):
+    pass
+
+class C(B):
+    pass
+
+class D(C):
+    pass
+
+for cls in [B, C, D]:
+    try:
+        raise cls()
+    except D:
+        print("D")
+    except C:
+        print("C")
+    except B:
+        print("B")
+
+try:
+    raise Exception('spam', 'eggs')
+except Exception as inst:
+    print(type(inst))
+    print(inst.args)
+    print(inst)
+
+    x, y = inst.args
+    print('x =', x)
+    print('y =', y)
