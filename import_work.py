c06af94228e2af808ea473152e8aada208314fe0
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-04-24 23:55:35 +0530

    Add import work file

---
+++ import_work.py
@@ -0,0 +1,16 @@
+# import math
+# result = math.sqrt(9)
+# print(result)
+
+# from math import sqrt
+
+# result = sqrt(9)
+# print(result)
+
+# from math import *
+
+# result = sqrt(9) * pi
+# print(result)
+
+import math
+print(dir(math))
