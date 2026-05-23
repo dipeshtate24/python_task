47b9748c03bd1eb6e97f079ae782a7e47e036751
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-05-23 23:53:17 +0530

    Update Multiple_inheritance.py

--- Multiple_inheritance.py
+++ Multiple_inheritance.py
@@ -17,3 +17,28 @@ p = Programmer()
 p.upgrade_level()
 print(p.level)
 print(p.company)
+
+class Employee:
+    def __init__(self, name):
+        self.name = name
+
+    def showName(self):
+        print(f"The name of Empolyee is {self.name}")
+        
+
+class Dancer:
+    def __init__(self, dance):
+        self.dance = dance
+
+    def showDance(self):
+        print(f"The performing dance form is {self.dance}")
+
+class DancerEmployee(Employee, Dancer):
+    def __init__(self, dance, name):
+        self.dance = dance
+        self.name = name
+
+o = DancerEmployee('Kathak', 'Pooja')
+o.showName()
+o.showDance()
+print(DancerEmployee.mro())
