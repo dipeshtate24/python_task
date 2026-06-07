5f188964ef5cba93a1c4f19377fa6d7a80490147
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-06-07 21:01:09 +0530

    Add AsyncIO file

---
+++ AsyncIO.py
@@ -0,0 +1,17 @@
+import time 
+
+def function1():
+    time.sleep(3)
+    print("func 1")
+
+def function2():
+    time.sleep(3)
+    print("func 2")
+
+def function3():
+    time.sleep(3)
+    print("func 3")
+
+function1()
+function2()
+function3()
