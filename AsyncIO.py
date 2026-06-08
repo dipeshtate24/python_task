58de9fd0b93d5a5660d08faa754e4db0cf7aa91d
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-06-08 23:44:56 +0530

    Update AsyncIO.py

--- AsyncIO.py
+++ AsyncIO.py
@@ -1,4 +1,6 @@
 import time 
+import asyncio
+
 
 def function1():
     time.sleep(3)
@@ -15,3 +17,29 @@ def function3():
 function1()
 function2()
 function3()
+
+async def function1():
+    await asyncio.sleep(1)
+    print("func 1")
+
+async def function2():
+    await asyncio.sleep(1)
+    print("func 2")
+
+async def function3():
+    await asyncio.sleep(4)
+    print("func 3")
+
+async def main():
+    L = await asyncio.gather(
+        function1(),
+        function2(),
+        function3()
+    )
+    print(L)
+    # task = asyncio.create_task(function1())
+    # # await function1()
+    # await function2()
+    # await function3()
+
+asyncio.run(main())
