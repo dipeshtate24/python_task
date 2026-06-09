e7ac425c3b47f484fc806c0fe64b992ba4cb4f68
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-06-09 23:41:58 +0530

    Update AsyncIO.py

--- AsyncIO.py
+++ AsyncIO.py
@@ -1,5 +1,6 @@
 import time 
 import asyncio
+import requests
 
 
 def function1():
@@ -43,3 +44,42 @@ async def main():
     # await function3()
 
 asyncio.run(main())
+
+async def function1():
+    url = "https://instagram.com/favicon.ico"
+    response = requests.get(url)
+    open("instagram1.ico", "wb").write(response.content)
+    await asyncio.sleep(1)
+    print("func 1")
+
+async def function2():
+    url = "https://instagram.com/favicon.ico"
+    response = requests.get(url)
+    open("instagram2.ico", "wb").write(response.content)
+    await asyncio.sleep(1)
+    print("func 2")
+
+async def function3():
+    url = "https://instagram.com/favicon.ico"
+    response = requests.get(url)
+    open("instagram3.ico", "wb").write(response.content)
+    await asyncio.sleep(4)
+    print("func 3")
+
+async def main():
+    await function1()
+    await function2()
+    await function3()
+    # L = await asyncio.gather(
+    #     function1(),
+    #     function2(),
+    #     function3()
+    # )
+    # print(L)
+    # task = asyncio.create_task(function1())
+    # # await function1()
+    # await function2()
+    # await function3()
+
+asyncio.run(main())
+
