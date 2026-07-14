96e8f45aec72477d5a4dc12d6a102497aa3b7bbf
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-07-14 23:44:33 +0530

    Update pratice_control_flow.py

--- pratice_control_flow.py
+++ pratice_control_flow.py
@@ -1,34 +1,34 @@
-# x = int(input('Enter your number: '))
+x = int(input('Enter your number: '))
 
-# if x < 0 :
-#     x = 0
-#     print('Negative Change to zero')
+if x < 0 :
+    x = 0
+    print('Negative Change to zero')
 
-# elif x == 0:
-#     print('zero')
+elif x == 0:
+    print('zero')
 
-# elif x == 1:
-#     print('one')
+elif x == 1:
+    print('one')
 
-# else:
-#     print('More')
+else:
+    print('More')
 
-# words = ['cat', 'window', 'defenestrate']
-# for w in words:
-#     print(w, len(w))
+words = ['cat', 'window', 'defenestrate']
+for w in words:
+    print(w, len(w))
 
-# users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
+users = {'Hans': 'active', 'Éléonore': 'inactive', '景太郎': 'active'}
 
-# for user, status in users.copy().items():
-#     if status == "inactive":
-#         del users[user]
+for user, status in users.copy().items():
+    if status == "inactive":
+        del users[user]
 
-# active_user = {}
-# for user, status in users.items():
-#     if status == 'active':
-#         active_user[user] = status
+active_user = {}
+for user, status in users.items():
+    if status == 'active':
+        active_user[user] = status
 
-# print(active_user)
+print(active_user)
 
 a = ['Mary', 'had', 'a', 'little', 'lamb']
 for i in range(len(a)):
