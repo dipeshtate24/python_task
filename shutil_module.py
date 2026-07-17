d1166bb75206f996789908ed027bea08a3f98ea0
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-07-17 23:50:18 +0530

    Update shutil_module.py

--- shutil_module.py
+++ shutil_module.py
@@ -1,12 +1,12 @@
 import shutil
 import os
 
-# shutil.copy('sample.txt', 'sample1.txt')
+shutil.copy('sample.txt', 'sample1.txt')
 
-# shutil.copytree('Image_folder', 'Image_directory')
+shutil.copytree('Image_folder', 'Image_directory')
 
-# shutil.move("1.png", "Image_folder/1.png")
+shutil.move("1.png", "Image_folder/1.png")
 
-# shutil.rmtree('Image_directory')
+shutil.rmtree('Image_directory')
 
 os.remove('sample1.txt')
