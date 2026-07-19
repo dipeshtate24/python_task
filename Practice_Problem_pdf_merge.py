1bdbba1e4127ed7fe3f6057003507808d1221e73
dipeshtate24 <dipeshtate24@gmail.com>
2026-07-19 21:11:07 +0530

    updated pdf merge code

--- Practice_Problem_pdf_merge.py
+++ Practice_Problem_pdf_merge.py
@@ -3,12 +3,16 @@ from pypdf import PdfWriter
 
 merger = PdfWriter()
 
-folder_path = 'pdf_folder'
+folder_path = r'C:\Users\Dipesh\Documents\Educational book & PDF\Dipesh doc\EaseMyAI_payment_slip\last_3_month_salary'
 
-for files in os.listdir(folder_path):
-    if files.endswith('.pdf'):
-        
-        for pdf in files :
-            merger(pdf)
+merger = PdfWriter()
 
-merger.write("new_file.pdf")
+for file in os.listdir(folder_path):
+    if file.endswith(".pdf"):
+        pdf_path = os.path.join(folder_path, file)
+        merger.append(pdf_path)
+
+with open("last_3_months_salary.pdf", "wb") as output:
+    merger.write(output)
+
+merger.close()
