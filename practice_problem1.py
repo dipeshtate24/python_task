972d25916cbda40b02518f7d7ad100d5047f7b58
dipeshtate24 <dipeshtate24@gmail.com>
2026-08-16 23:48:28 +0530

    update practice_problem1.py

--- practice_problem1.py
+++ practice_problem1.py
@@ -1,4 +1,3 @@
-
 import random
 list_number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 count = 0
@@ -13,3 +12,36 @@ for i in range(0, len(list_number)):
     else:
         print('guess number is not correct. please try again.')
         count += 1
+
+
+def min_max(lst):
+    new_list = []
+    if len(lst) > 1:
+        new_list.append(min(lst))
+        new_list.append( max(lst))
+        return new_list
+    
+    else:
+        new_list.append(min(lst))
+        new_list.append(1)
+        return new_list
+
+print(min_max([1]))
+
+def min_max(lst):
+  return [min(lst), max(lst)]
+
+
+def persistence(n):
+    count = 0
+
+    while n >= 10:
+        product = 1
+        
+        for digit in str(n):
+            product *= int(digit)
+        
+        n = product
+        count += 1
+    return print(count)
+persistence(39)
