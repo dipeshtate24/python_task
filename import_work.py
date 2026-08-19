c074fcab310f0282475852ecfbd72d693284bf22
Dipesh Tate <74396369+dipeshtate24@users.noreply.github.com>
2026-08-19 23:40:53 +0530

    Update import_work.py

--- import_work.py
+++ import_work.py
@@ -1,16 +1,16 @@
-# import math
-# result = math.sqrt(9)
-# print(result)
+import math
+result = math.sqrt(9)
+print(result)
 
-# from math import sqrt
+from math import sqrt
 
-# result = sqrt(9)
-# print(result)
+result = sqrt(9)
+print(result)
 
-# from math import *
+from math import *
 
-# result = sqrt(9) * pi
-# print(result)
+result = sqrt(9) * pi
+print(result)
 
 import math
 print(dir(math))
